b04c8b5bd73a93142dffbd57727be1e6de5e81f3
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-02-25 19:36:27 +0100

    made changes

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app">
-    
+    <AppNav />
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
---
+++ components/App/Nav.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav>
+    Testing nav
+  </nav>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>

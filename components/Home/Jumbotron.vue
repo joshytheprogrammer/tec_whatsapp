b38da82be855f08ab588dae3f34e4101a75a4c3a
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-02-25 21:55:05 +0100

    made changes

--- components/Home/Jumbotron.vue
+++ components/Home/Jumbotron.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-
-  </div>
+  <section class="hero">
+    <img src="" alt="TEC Whatsapp Banner " id="banner_ad_image">
+  </section>
 </template>
 
 <script setup>
@@ -9,5 +9,22 @@
 </script>
 
 <style lang="scss" scoped>
+.hero {
+  margin: 1rem 3rem 0 3rem;
 
+  #banner_ad_image {
+    width: 100%;
+    height: 300px;
+    object-fit: fill;
+    border-radius: $standard-r;
+  }
+
+  @media screen and (max-width: $medium) {
+    margin: 12px 14px 0 14px;
+
+    #banner_ad_image {
+      height: 200px;
+    }
+  }
+}
 </style>

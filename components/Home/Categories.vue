13f68515bd91cb53498b5c7860cd30e82f530f44
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-02-26 22:08:56 +0100

    made changes

--- components/Home/Categories.vue
+++ components/Home/Categories.vue
@@ -3,9 +3,10 @@
     <AppHeaders>
       <template #title>Popular Categories</template>
     </AppHeaders>
-    <AppLoader v-if="$fetchState.pending" />
-    <AppNetworkError v-else-if="$fetchState.error || error" :message="error" />
-    <div v-else class="categories">
+    <!-- <AppLoader v-if="$fetchState.pending" />
+    <AppNetworkError v-else-if="$fetchState.error || error" :message="error" /> -->
+    <!-- Don't forget to add v-else -->
+    <div class="categories">
       <AppCardsCategory v-for="item in categories" :key="item.id" :item="item" />
     </div>
   </div>
@@ -18,21 +19,23 @@ let categories = [
     id: 0,
     name: "Chocolate cake",
     thumbnail: "https://images.unsplash.com/photo-1606313564200-e75d5e30476c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
-    slug: "chocolate-cake"
+    slug: "#chocolate-cake"
   },
   {
     id: 1,
     name: "Birthday Cakes",
     thumbnail: "https://images.unsplash.com/photo-1571622840901-92ae138bd36e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
-    slug: "birthday-cakes"
+    slug: "#birthday-cakes"
   },
   {
     id: 2,
     name: "Wedding Cakes",
     thumbnail: "https://images.unsplash.com/photo-1623428454614-abaf00244e52?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
-    slug: "wedding-cakes"
+    slug: "#wedding-cakes"
   }
 ];
+
+
 </script>
 
 <style lang="scss" scoped>

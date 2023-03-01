9e4e89c9d4de54a40b88a0c52f395f9dde7a8ebb
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-01 17:10:07 +0100

    made changes

--- components/Home/Jumbotron.vue
+++ components/Home/Jumbotron.vue
@@ -1,50 +1,50 @@
 <template>
   <section class="hero">
-    <img src="https://firebasestorage.googleapis.com/v0/b/tec-whatsapp.appspot.com/o/Banners%2FFirst%20TEC%20banner%20-%20Desktop.png?alt=media&token=7a23e4b5-47e5-4760-a021-9e15b06bd98e" alt="TEC Whatsapp Banner " id="banner_ad_image">
+    <img :src="banner" alt="TEC Whatsapp Banner " id="banner_ad_image">
   </section>
 </template>
 
 <script setup>
-// import { useMobileStore } from '@/store/mobile'
+import { useMobileStore } from '@/store/mobile'
 
-// // Get device details from store
-// const device = useMobileStore()
+// Get device details from store
+const device = useMobileStore()
 
-// let currentNo = 0
+let currentNo = 0
 
-// // Will contain current banner
-// let banner = ''
+// Will contain current banner
+let banner = ref('')
 
-// // Contain array of all banners for that device
-// let banners = []
+// Contain array of all banners for that device
+let banners = []
 
-// // Contains all banners for desktop
-// let desktopBanners = [
-//   "https://firebasestorage.googleapis.com/v0/b/tec-whatsapp.appspot.com/o/Banners%2FFirst%20TEC%20banner%20-%20Desktop.png?alt=media&token=7a23e4b5-47e5-4760-a021-9e15b06bd98e"
-// ]
+// Contains all banners for desktop
+let desktopBanners = [
+  "https://firebasestorage.googleapis.com/v0/b/tec-whatsapp.appspot.com/o/Banners%2FFirst%20TEC%20banner%20-%20Desktop.png?alt=media&token=7a23e4b5-47e5-4760-a021-9e15b06bd98e"
+]
 
-// // Contains all banners for mobile
-// let mobileBanners = [
-//   "https://firebasestorage.googleapis.com/v0/b/neas-fashion.appspot.com/o/Banner%2FNeas%20fashion%20banner%20ad%20-%20mobile.png?alt=media&token=d71b410b-06f3-49f4-8f2c-d93c1bf0734e"
-// ]
+// Contains all banners for mobile
+let mobileBanners = [
+  "https://firebasestorage.googleapis.com/v0/b/tec-whatsapp.appspot.com/o/Banners%2FFirst%20TEC%20Design%20-%20Mobile.png?alt=media&token=5b24a2d4-24a7-4028-abe7-f44a9417f13d",
+]
 
-// onMounted(() => {
-//   device.getDevice ? banners = mobileBanners : banners = desktopBanners
+onMounted(() => {
+  device.getDevice ? banners = mobileBanners : banners = desktopBanners
 
-//   banner = banners[0]
+  banner.value = banners[0]
 
-//   setInterval(() => {
-//     doSlideShow()
-//   }, 5000)
-// });
+  setInterval(() => {
+    doSlideShow()
+  }, 5000)
+});
 
-// function doSlideShow() {
-//   if(currentNo > (banners.length - 1)) { currentNo = 0 }
+function doSlideShow() {
+  if(currentNo > (banners.length - 1)) { currentNo = 0 }
 
-//   banner = banners[currentNo]
+  banner.value = banners[currentNo]
 
-//   currentNo++
-// }
+  currentNo++
+}
 </script>
 
 <style lang="scss" scoped>
@@ -59,7 +59,7 @@
   #banner_ad_image {
     max-width: 100%;
     max-height: 300px;
-    object-fit: contain;
+    object-fit: fill;
     border-radius: $standard-r;
   }
 

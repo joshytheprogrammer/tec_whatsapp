950c3083e1f755102016d2fb65cd53cbc83bdc84
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-08 17:14:39 +0100

    made changes

---
+++ server/api/categories/products/[products].get.js
@@ -0,0 +1,26 @@
+import { getFirestore } from 'firebase-admin/firestore'
+import {initializeApp, getApps, cert} from "firebase-admin/app"
+
+const apps = getApps()
+
+if (!apps.length) {
+  initializeApp({
+    credential: cert('./tec-whatsapp-firebase-adminsdk-qei58-dfa66bbcb1.json')
+  })
+}
+
+export default defineEventHandler(async (event) => {
+  const db = getFirestore()
+  const querySnap = await db.collection('products')
+  .where("categories", "array-contains", event.context.params.products)
+  .get()
+
+  const productsData = querySnap.docs.map(doc => {
+    return {
+      uuid: doc.id,
+      ...doc.data()
+    }
+  })
+  
+  return productsData
+})

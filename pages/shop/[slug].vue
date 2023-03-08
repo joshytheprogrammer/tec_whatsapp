733d05ab22db06b14d2c49af356169712f71fbda
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-08 11:47:31 +0100

    made changes

---
+++ pages/shop/[slug].vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="product">
+    <AppLoader v-if="pending" />
+    <AppNetworkError v-else-if="error" :message="error" />
+    {{ product }}
+  </div>
+</template>
+
+<script setup>
+const route = useRoute();
+
+const { data: product, pending, error } = await useFetch('/api/products/'+route.params.slug );
+</script>
+
+<style lang="scss" scoped>
+
+</style>

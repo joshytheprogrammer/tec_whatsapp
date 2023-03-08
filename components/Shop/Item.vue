91c1cc060e0d38bb1f49652d9e7610ee61cac44f
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-08 12:35:21 +0100

    made changes

--- components/Shop/Item.vue
+++ components/Shop/Item.vue
@@ -2,16 +2,27 @@
   <div class="item">
     <img :src="product.thumbnail" :alt="'thumbnail - ' + product.slug">
     <div class="_details">
-
+      <div class="_default">
+        <h1> {{product.name}} </h1>
+        <p :title="product.name + ' costs ' + product.price">{{formatter(product.price)}}</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-  const props = defineProps(['product']);
-  const product = props.product;
+const props = defineProps(['product']);
+const product = props.product[0];
 
-  console.log(product.thumbnai)
+function formatter(price) {
+  let currency = '₦'
+
+  let formatter = new Intl.NumberFormat('en-US');
+
+  price = currency + formatter.format(price)
+
+  return price
+}
 </script>
 
 <style lang="scss" scoped>

ea2be47bb7a572222d75b1920f9d000b49f5f17a
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-08 12:18:27 +0100

    made changes

--- components/Shop/Item.vue
+++ components/Shop/Item.vue
@@ -1,15 +1,21 @@
 <template>
-  <div>
+  <div class="item">
+    <img :src="product.thumbnail" :alt="'thumbnail - ' + product.slug">
+    <div class="_details">
 
+    </div>
   </div>
 </template>
 
-<script>
-  export default {
-    
-  }
+<script setup>
+  const props = defineProps(['product']);
+  const product = props.product;
+
+  console.log(product.thumbnai)
 </script>
 
 <style lang="scss" scoped>
+.item {
 
+}
 </style>
